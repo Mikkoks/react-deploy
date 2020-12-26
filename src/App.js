f33e8aba3d3623ca13c57745c9bd007f5a397cdc
mikkoks <mikkoksaarinen@gmail.com>
2020-12-27 01:45:42 +0200

    deploy riact up to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import peppiImage from './img/peppi.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ma react up</h1>
+      <h1>Mikko</h1>
+      <img src={peppiImage} alt="peppiImage" />
+
     </div>
   );
 }
